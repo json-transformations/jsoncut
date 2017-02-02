837178ca851e1d68dd236a1902d08f8f0775a87b
Brian Peterson <bpeterso2000@yahoo.com>
2017-02-01 19:47:00 -0500

    Added docstrings

--- jsoncut/exceptions.py
+++ jsoncut/exceptions.py
@@ -8,9 +8,25 @@ from . import core
 Color = namedtuple('Color', ['val', 'style'])
 
 
-def color_error_mesg(fmtstr, kwds, nocolor=False):
-    """asterix before color name indicates bold."""
-    if nocolor:
+def color_error_mesg(fmtstr, kwds, no_color=False):
+    """Colorize the error message.
+
+    Args:
+        fmstr (str): see Python format string mini-language ...
+            https://docs.python.org/3.6/library/string.html#formatspec
+        kwds (Mapping): the keys are the names of the keywords to
+            display; the values are the color names (see below)
+        no_color (bool): disable colors.
+
+    Returns:
+        str: colorized message.
+
+    Supported colors:
+        * black, red, green, yellow, blue, magenta, cyan, white.
+        * to make a color bold prefix it with an asterix (*)
+
+    """
+    if no_color:
         kwds = {k: v.val for k, v in kwds.items()}
     else:
         def get_style(c):
@@ -20,35 +36,50 @@ def color_error_mesg(fmtstr, kwds, nocolor=False):
     return fmtstr.format(**kwds)
 
 
-def default_error_mesg_fmt(exc, colorless=False):
+def default_error_mesg_fmt(exc, no_color=False):
+    """Generate a default error message for custom exceptions.
+
+    Args:
+        exc (Exception): the raised exception.
+        no_color (bool): disable colors.
+
+    Returns:
+        str: colorized error message.
+    """
     return color_error_mesg('{err_name}: {err_mesg}', {
         'err_name': Color(exc.__class__.__name__, '*red'),
         'err_mesg': Color(str(exc), 'white')
-    }, colorless)
+    }, no_color)
 
 
 class JsonCutError(Exception):
     """JSON Cut Base Exception."""
 
     def format_error(self, nocolor=False):
+        """Generate formatted error message."""
         return default_error_mesg_fmt(self, nocolor)
 
 
 class KeyNumberOutOfRange(JsonCutError, ValueError):
     """Invalid Key-Number."""
+
     pass
 
 
 class KeyTypeError(JsonCutError, TypeError):
-    """The key or index was not found in the JSON document."""
+    """Attempt to use an index on a Mapping or a key on a Sequence."""
+
     def __init__(self, exc, op=None, key=None, itemnum=0, data=None,
                  keylist=None):
-        """Initialize KeyNotFound Exception.
+        """Initialize KeyNumberOutOfRange Exception.
 
-        Kwds:
-            fn (str): name of module/funct where exception was raised
-            item (self):  JSON data from which the key was missing
-        :param keylist: a list of available keys
+        Args:
+            exc (Exception): orignial TypeError excecption.
+            op (str): name of operation where the exception occured.
+            key (str): the key being operated on.
+            itemnum (int): the data item number; if data is a Sequence.
+            data (self):  JSON data being operated on.
+            keylist (List[str]): a list of keys found in the data.
         """
         msg = str(exc)
         super(KeyTypeError, self).__init__(msg)
@@ -58,6 +89,7 @@ class KeyTypeError(JsonCutError, TypeError):
         self.keylist = keylist
 
     def format_error(self, nocolor=False):
+        """Generate formatted error message."""
         mesg = (
             '{dashed_line}\n'
             '{error}\n'
@@ -77,15 +109,18 @@ class KeyTypeError(JsonCutError, TypeError):
 
 
 class IndexOfOfRange(JsonCutError, IndexError):
-    """The key or index was not found in the JSON document."""
+    """The index number exceeded the length of the sequence."""
 
     def __init__(self, exc, op=None, itemnum=0, data=None, keylist=None):
-        """Initialize KeyNotFound Exception.
+        """Initialize IndexOutOfRange Exception.
 
-        Kwds:
-            fn (str): name of module/funct where exception was raised
-            item (self):  JSON data from which the key was missing
-        :param keylist: a list of available keys
+        Args:
+            exc (Exception): orignial TypeError excecption.
+            op (str): name of operation where the exception occured.
+            key (str): the key being operated on.
+            itemnum (int): the data item number; if data is a Sequence.
+            data (self):  JSON data being operated on.
+            keylist (List[str]): a list of keys found in the data.
         """
         msg = str(exc)
         super(KeyNotFound, self).__init__(msg)
@@ -95,6 +130,7 @@ class IndexOfOfRange(JsonCutError, IndexError):
         self.keylist = keylist
 
     def format_error(self, nocolor=False):
+        """Generate formatted error message."""
         mesg = (
             '{dashed_line}\n'
             'IndexOutOfRange: {key_name}\n'
@@ -107,11 +143,9 @@ class IndexOfOfRange(JsonCutError, IndexError):
             '{dashed_line}\n'
             '{available_keys}\n'
         )
-        name = self.args[0]
-        key_name = name[1:-1] if len(name) > 2 else name
         available_keys = '\n'.join(list(core.list_keys(self.data)))
         kwds = {
-            'index_number': Color(key_name, '*red'),
+            'index_number': Color(self.args[0], '*red'),
             'dashed_line': Color('-' * 39, 'yellow'),
             'item_number': Color(self.item_number, 'red'),
             'operation': Color(self.operation, 'red'),
@@ -122,7 +156,7 @@ class IndexOfOfRange(JsonCutError, IndexError):
 
 
 class KeyNotFound(JsonCutError, KeyError):
-    """The key or index was not found in the JSON document."""
+    """The key was not found in the JSON document."""
 
     def __init__(self, exc, op=None, itemnum=0, data=None, keylist=None):
         """Initialize KeyNotFound Exception.
@@ -140,6 +174,7 @@ class KeyNotFound(JsonCutError, KeyError):
         self.keylist = keylist
 
     def format_error(self, nocolor=False):
+        """Generate formatted error message."""
         mesg = (
             '{dashed_line}\n'
             'KeyNotFound: {key_name}\n'
