100225511f229a829ed475fc31a483f03b995ef0
Brian Peterson <bpeterso2000@yahoo.com>
2017-02-14 21:21:33 -0500

    Added tests

---
+++ tests/test_core.py
@@ -0,0 +1,47 @@
+"""Test JSON Cut main functions."""
+import jsoncut.core
+
+TEST_DATA = {
+    'info': 'test',
+    'results': [
+        {
+            'id': 1719,
+            'via': {
+                'channel': 'email',
+                'source': {
+                    'from': {'name': 'John Doe'}
+                }
+            }
+        },
+        {
+            'id': 1720,
+            'via': {
+                'channel': 'email',
+                'source': {
+                    'from': {'name': 'Jane Doe'}
+                }
+            }
+        }
+    ]
+}
+
+PRUNED_TEST_DATA = [
+    {
+        'id': 1719,
+        'source': {'from': {}}
+    },
+    {
+        'id': 1720,
+        'source': {'from': {}}
+    }
+]
+
+
+def test_cut():
+    """Test core.cut()."""
+    rootkey = 'results'
+    getkeys = 'id, via.source'
+    delkeys = 'source.from.name'
+    result = jsoncut.core.cut(TEST_DATA, rootkey=rootkey, getkeys=getkeys,
+                              delkeys=delkeys)
+    assert result == PRUNED_TEST_DATA
