611434920c189a759eccda7045ce4813ced0fa5e
Timoteo <phillipstr@gmail.com>
2017-09-10 22:42:33 -0400

    started flattener.py

--- jsoncut/flattener.py
+++ jsoncut/flattener.py
@@ -1,3 +1,19 @@
 """
 JSON file flattener
 """
+
+from .core import list_keys
+from .tokenizer import SLICE_RE
+from .inspector import inspect_json, count_arrays
+
+
+
+def flatten_all(d):
+    keys = [item.split(' ')[-1] for item in list_keys(d)]
+    pass
+
+
+def flatten_by_keylist(d, keys=None):
+    if keys == None:
+        return flatten_all(d)
+    pass
